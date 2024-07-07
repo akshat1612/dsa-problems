eb4ecd8e8235fdd6a69c4f1b1e6ac6e9524fbe4d
Akshat Singhal <akshat.singhal@tsgforce.com>
2024-07-08 00:54:06 +0530

    Recursion - LeetCode 90

--- recursion/LC90_Subsets2.java
+++ recursion/LC90_Subsets2.java
@@ -7,21 +7,18 @@ import java.util.List;
 public class LC90_Subsets2 {
     public static void main(String[] args) {
         int[] candidates = {1, 2, 2, 3, 3};
-        Solution solution = new Solution();
-        List<List<Integer>> lists = solution.subsetsWithDup(candidates);
+        List<List<Integer>> lists = subsetsWithDup(candidates);
         System.out.println(lists);
     }
-}
 
-class Solution {
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
+    static List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
         subsets(0, nums, new ArrayList<>(), ans);
         return ans;
     }
 
-    void subsets(int index, int[] nums, List<Integer> ds, List<List<Integer>> ans) {
+    static void subsets(int index, int[] nums, List<Integer> ds, List<List<Integer>> ans) {
         ans.add(new ArrayList<>(ds));
         if (index == nums.length) {
             return;
