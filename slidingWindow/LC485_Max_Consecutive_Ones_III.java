755f17b14dcc0274914fb8b2c0484607e94fd9c1
Akshat Singhal <akshat.yash.singhal@gmail.com>
2024-07-13 16:43:46 +0530

    LC 1004. Max Consecutive Ones - III

---
+++ slidingWindow/LC485_Max_Consecutive_Ones_III.java
@@ -0,0 +1,33 @@
+package slidingWindow;
+
+public class LC485_Max_Consecutive_Ones_III {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0};
+        System.out.println(longestOnes(nums, 2));
+
+        int[] nums2 = {0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1};
+        System.out.println(longestOnes(nums2, 3));
+    }
+
+    static int longestOnes(int[] nums, int k) {
+        int count0 = 0;
+        int ans = 0;
+        int r = 0;
+        int l = 0;
+
+        while (r < nums.length) {
+            if (nums[r] == 0) {
+                count0++;
+            }
+            if (count0 > k) {
+                if (nums[l] == 0) count0--;
+                l++;
+            }
+            if (count0 <= k) {
+                ans = Math.max(ans, r - l + 1);
+            }
+            r++;
+        }
+        return ans;
+    }
+}
