02ac979638b007020377526c43df14333d6f6aad
Akshat Singhal <akshat.yash.singhal@gmail.com>
2024-07-29 01:24:56 +0530

    LC206: reverse Linked list

---
+++ linkedLists/LC19_Remove_from_Nth_Linked_List.java
@@ -0,0 +1,26 @@
+package linkedLists;
+
+public class LC19_Remove_from_Nth_Linked_List {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))));
+        System.out.println(l1.print(l1));
+        System.out.println();
+        System.out.println(l1.print(reverseList(l1)));
+    }
+
+    static ListNode reverseList(ListNode head) {
+        return reverse(head).h;
+    }
+
+    static Pair reverse(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return new Pair(head, head);
+        }
+        ListNode n = head.next;
+        head.next = null;
+        Pair rev = reverse(n);
+        rev.t.next = head;
+        return new Pair(rev.h, head);
+    }
+}
