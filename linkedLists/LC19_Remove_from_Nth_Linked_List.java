77a388200301a5925c6d9d792b6d3e28ba4bea03
Akshat Singhal <akshat.yash.singhal@gmail.com>
2024-07-29 01:25:16 +0530

    LC19: remove from last Nth position in Linked list

--- linkedLists/LC19_Remove_from_Nth_Linked_List.java
+++ linkedLists/LC19_Remove_from_Nth_Linked_List.java
@@ -3,24 +3,33 @@ package linkedLists;
 public class LC19_Remove_from_Nth_Linked_List {
     public static void main(String[] args) {
         ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))));
+        ListNode l2 = new ListNode(1, new ListNode(4, new ListNode(2, new ListNode(5, new ListNode(8, new ListNode(3))))));
         System.out.println(l1.print(l1));
+        System.out.println(l1.print(removeNthFromEnd(l1, 3)));
         System.out.println();
-        System.out.println(l1.print(reverseList(l1)));
+        System.out.println(l2.print(l2));
+        System.out.println(l2.print(removeNthFromEnd(l2, 2)));
     }
 
-    static ListNode reverseList(ListNode head) {
-        return reverse(head).h;
-    }
-
-    static Pair reverse(ListNode head) {
-
-        if (head == null || head.next == null) {
-            return new Pair(head, head);
+    static ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head.next == null) {
+            return null;
         }
-        ListNode n = head.next;
-        head.next = null;
-        Pair rev = reverse(n);
-        rev.t.next = head;
-        return new Pair(rev.h, head);
+        ListNode fast = head, slow = head;
+        int i = 0;
+        while (i < n) {
+            fast = fast.next;
+            i++;
+        }
+        if (fast == null) {
+            return slow.next;
+        }
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
     }
+
 }
