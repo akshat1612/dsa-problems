b5bd6619be6eb8d6907159fd20ad18f1ce8594e5
Akshat Singhal <akshat.yash.singhal@gmail.com>
2024-08-13 23:56:55 +0530

    GFG: Flatten Linked list

--- linkedLists/GFG_Flatten_LinkedList.java
+++ linkedLists/GFG_Flatten_LinkedList.java
@@ -21,16 +21,41 @@ class Node3 {
         StringBuilder a = new StringBuilder();
         while (head != null) {
             a.append(head.data).append(" --> ");
-            head = head.next;
+            head = head.bottom;
         }
         return a.substring(0, a.length() - 5);
     }
 }
 public class GFG_Flatten_LinkedList {
     public static void main(String[] args) {
-//        Node3
-//        System.out.println(l1.toString(l1));
-//        System.out.println(l1.toString(segregate(l1)));
+        Node3 node1 = new Node3(1);
+        Node3 node2 = new Node3(2);
+        Node3 node3 = new Node3(3);
+        Node3 node4 = new Node3(4);
+        Node3 node5 = new Node3(5);
+        Node3 node6 = new Node3(6);
+        Node3 node7 = new Node3(7);
+        Node3 node8 = new Node3(8);
+        Node3 node9 = new Node3(9);
+        Node3 node10 = new Node3(10);
+        Node3 node11 = new Node3(11);
+        Node3 node12 = new Node3(12);
+
+        node2.bottom = node10;
+        node1.bottom = node7;
+        node7.bottom = node11;
+        node11.bottom = node12;
+        node4.bottom = node9;
+        node5.bottom = node6;
+        node6.bottom = node8;
+        node3.next = node2;
+        node2.next = node1;
+        node1.next = node4;
+        node4.next = node5;
+
+        System.out.println(node3.toString(node3));
+        System.out.println(node3.toString(flatten(node3)));
+
     }
 
     static Node3 flatten(Node3 root) {
